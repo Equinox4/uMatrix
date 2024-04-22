260838682bc5eb0fde827676cef042b3ec8302b8
Mjöllnir <github+perso@mjollnir.fr>
2024-04-22 10:57:42 +0200

    update: apply last PRs
    - display correctly long domain names
    - add a message
    - remove obsolete filters
    - fix build scripts
    - fix typos in README

--- tools/make-assets.sh
+++ tools/make-assets.sh
@@ -14,20 +14,21 @@ cp ./assets/assets.json $DES/
 if [ -n "${TRAVIS_TAG}" ]; then
   pushd .. > /dev/null
   git clone --depth 1 https://github.com/uBlockOrigin/uAssets.git
-  git checkout 84dc2761abb4193bb34290aa6d90266610f735f6
+  git checkout master
   popd > /dev/null
 fi
 
 mkdir $DES/thirdparties
 pushd ../uAssets
-git checkout 84dc2761abb4193bb34290aa6d90266610f735f6
+git checkout master
 popd
-cp -R ../uAssets/thirdparties/mirror1.malwaredomains.com $DES/thirdparties/
+#cp -R ../uAssets/thirdparties/mirror1.malwaredomains.com $DES/thirdparties/
+cp -R ../uAssets/thirdparties/easylist                   $DES/thirdparties/
 cp -R ../uAssets/thirdparties/pgl.yoyo.org               $DES/thirdparties/
 cp -R ../uAssets/thirdparties/publicsuffix.org           $DES/thirdparties/
-cp -R ../uAssets/thirdparties/someonewhocares.org        $DES/thirdparties/
-cp -R ../uAssets/thirdparties/winhelp2002.mvps.org       $DES/thirdparties/
-cp -R ../uAssets/thirdparties/www.malwaredomainlist.com  $DES/thirdparties/
+#cp -R ../uAssets/thirdparties/someonewhocares.org        $DES/thirdparties/
+#cp -R ../uAssets/thirdparties/winhelp2002.mvps.org       $DES/thirdparties/
+#cp -R ../uAssets/thirdparties/www.malwaredomainlist.com  $DES/thirdparties/
 mkdir $DES/umatrix
 cp -R ../uAssets/recipes/*                               $DES/umatrix/
 pushd ../uAssets
